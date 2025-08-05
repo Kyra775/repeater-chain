a25a061dcb09a5075e86e50d7a36ec580c8b06e7
Kyra <ikansarden558@gmail.com>
2025-08-06 00:11:45 +0800

    Create password_hash.c

---
+++ crypto_core/password_hash.c
@@ -0,0 +1,24 @@
+#include <sodium.h>
+#include <stdio.h>
+#include <string.h>
+
+// Fungsi: convert password to keypair
+// Output: private_key (64 bytes) dan public_key (32 bytes)
+void password_to_keypair(const char *password, unsigned char *public_key, unsigned char *private_key) {
+    unsigned char seed[crypto_generichash_BYTES];
+    
+    // Hash password menjadi seed 32 byte
+    crypto_generichash(seed, sizeof seed, (const unsigned char*)password, strlen(password), NULL, 0);
+    
+    // Generate keypair dari seed
+    crypto_sign_seed_keypair(public_key, private_key, seed);
+}
+
+// Fungsi login: verifikasi password cocok dengan identitas
+int verify_password(const char *password, const unsigned char *stored_public_key) {
+    unsigned char test_public_key[32];
+    unsigned char test_private_key[64];
+    password_to_keypair(password, test_public_key, test_private_key);
+    
+    return memcmp(stored_public_key, test_public_key, 32) == 0;
+}
